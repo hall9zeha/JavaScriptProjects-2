0ddbeee8321f44f3c7dd787961819ab07e9aab1c
hall9zeha <vmwaretars@gmail.com>
2024-01-04 10:24:13 -0500

    Created gifExpertApp

---
+++ 02-gif-expert-app/src/GifExpertApp.js
@@ -0,0 +1,14 @@
+
+import React, { memo } from 'react'
+
+const GifExpertApp = memo(() => {
+  return (
+    <>
+    <h2>GifExpertApp</h2>
+    <hr/>
+    </>
+    
+  )
+})
+
+export default GifExpertApp
