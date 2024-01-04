d073f853e9aba833a5f8036194e162d7d0a7c967
hall9zeha <vmwaretars@gmail.com>
2024-01-04 11:16:11 -0500

    Added addCategory component

--- 02-gif-expert-app/src/GifExpertApp.js
+++ 02-gif-expert-app/src/GifExpertApp.js
@@ -1,11 +1,31 @@
 
-import React, { memo } from 'react'
+import React, { memo, useState } from 'react'
+import AddCategory from './components/AddCategory';
+
+
 
 const GifExpertApp = memo(() => {
+
+  const [categories, setCategory] = useState(['One punch', 'Violet Evergarden', 'Scavengers Reign']);
+
+  // const handleAdd = (e)=>{
+  //   setCategory([...categories,e]);//mantenemos el arreglo anterior y agregamos la nueva categoría al presionar el botón
+  // }
+
   return (
     <>
+
     <h2>GifExpertApp</h2>
+    <AddCategory/>
     <hr/>
+    
+    <ol>
+      {
+        categories.map(category =>{
+            return <li key={category}>{category}</li>
+        })
+      }
+    </ol>
     </>
     
   )
