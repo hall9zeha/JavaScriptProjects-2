bd8bd75b67e2630d8e2347c1f59810ae09e0ed22
hall9zeha <vmwaretars@gmail.com>
2024-01-05 10:37:19 -0500

    Added css animations

--- 02-gif-expert-app/src/components/GifListItem.js
+++ 02-gif-expert-app/src/components/GifListItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const GifListItem = ({title, url}) => {
   return (
-    <div className="card">
+    <div className="card animate__animated animate__bounceInLeft animate__delay-700s" >
         <img src={url} alt={title}/>
         <p>{title}</p>
         </div>
