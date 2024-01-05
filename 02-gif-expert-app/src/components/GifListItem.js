866b6f4efa21232326aa5312fc190e929405f080
hall9zeha <vmwaretars@gmail.com>
2024-01-05 13:01:17 -0500

    Added className matching test

--- 02-gif-expert-app/src/components/GifListItem.js
+++ 02-gif-expert-app/src/components/GifListItem.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 export const GifListItem = ({title, url}) => {
   return (
-    <div className="card animate__animated animate__bounceInLeft animate__delay-700s" >
+    <div  data-testid="div" className="card animate__animated animate__bounceInLeft animate__delay-700s" >
         <img src={url} alt={title}/>
         <p>{title}</p>
         </div>
--- 02-gif-expert-app/src/tests/components/GifListItem.test.js
+++ 02-gif-expert-app/src/tests/components/GifListItem.test.js
@@ -1,15 +1,38 @@
 import React from "react";
-import {render, screen} from '@testing-library/react';
+import {render, screen, fireEvent } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import { GifListItem } from "../../components/GifListItem";
 
 
 describe('Tests in GifLisItem', () => { 
-    test('should show GifListItem component successful', () => { 
-        const titleTest = 'Family spy';
-        const url='https//fakeurl.test.com/family.gif'
 
-        const wrapper = renderer.create(<GifListItem title={titleTest} url={url}/>).toJSON();
+    const titleTest = 'Family spy';
+    const url='https//fakeurl.test.com/family.gif'
+
+    const wrapper = renderer.create(<GifListItem title={titleTest} url={url}/>).toJSON();
+
+    test('should show GifListItem component successful', () => { 
+       
         expect(wrapper).toMatchSnapshot();
      })
- })
+    test('should have a title', () => { 
+       render(<GifListItem title={titleTest} url={url}/>)
+       expect(screen.getByText(titleTest)).toBeInTheDocument();
+       
+     })
+
+     test('should have a image component with "url" and "alt" props', () => { 
+        render(<GifListItem title={titleTest} url={url}/>)
+        expect(screen.getByRole("img")).toHaveAttribute('src',url);
+        expect(screen.getByRole("img")).toHaveAttribute('alt',titleTest);
+       
+      })
+
+      test('should have a animate__bounceInLeft in div', () => { 
+        const {container }=render(<GifListItem title={titleTest} url={url}/>)
+            
+        expect(container.firstChild).toHaveClass('animate__bounceInLeft');
+        
+       
+      })
+ }) 
