28c8557956131b35863a06f0d80a7adb35711660
hall9zeha <vmwaretars@gmail.com>
2024-01-09 09:54:19 -0500

    Added loaded gifs test

--- 02-gif-expert-app/src/tests/components/GifList.test.js
+++ 02-gif-expert-app/src/tests/components/GifList.test.js
@@ -1,9 +1,16 @@
 import React from "react";
 import renderer from 'react-test-renderer';
 import { GifList } from "../../components/GifList";
-
+import { useFetchGifs } from "../../hooks/useFetchGifs";
+jest.mock('../../hooks/useFetchGifs');//
 
 describe('testing in <GifList/> component', () => { 
+
+    //Simulamos la carga de imágenes con un mockReturnValue
+    useFetchGifs.mockReturnValue({
+        data:[],
+        loading:true
+    });
 
     const category = 'Scavengers reign';
     const wrapper = renderer.create(<GifList category={category}/>);
@@ -11,4 +18,22 @@ describe('testing in <GifList/> component', () => {
         const component = wrapper.toJSON();
         expect(component).toMatchSnapshot();
      })
+
+     test('shuld show items when are loading with useFetchGifs', ()=>{
+        const gifs = [{
+            id:'A123',
+            url:'https://fkeurl.com/fakeimg.jpg',
+            title:'Levi'
+        }]
+
+        useFetchGifs.mockReturnValue({
+            data:gifs,
+            loading:false
+        });
+
+        const wrapper = renderer.create(<GifList category={category}/>);
+
+        expect(wrapper).toMatchSnapshot();
+
+     });
  })
