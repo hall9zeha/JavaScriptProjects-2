5367ed4d677402e71a07bd4f9183164ecbcdbe68
hall9zeha <vmwaretars@gmail.com>
2024-01-11 11:24:21 -0500

    Added FocusScreen component

---
+++ 03-hook-app/src/components/04-useRef/FocusScreen.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import '../02-useEffect/effects.css';
+export const FocusScreen = () => {
+    const handleClick = ()=>{
+        document.querySelector('input').select();
+    }
+  return (
+    <div>
+        <h1>Focus Screen</h1>
+        <hr/>
+
+        <input 
+            className='form-control'
+            placeholder='Nombre'
+            />
+        <button
+            className='btn btn-outline-primary mt-3'
+            onClick={handleClick}
+            >
+            Focus
+        </button>
+
+    </div>
+  )
+}
--- 03-hook-app/src/index.js
+++ 03-hook-app/src/index.js
@@ -5,14 +5,13 @@ import { CounterCustomHook } from './components/01-useState/CounterCustomHook';
 import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 import { SimpleForm } from './components/02-useEffect/SimpleForm';
 import { MultipleCustomHooks } from './components/03-Examples/MultipleCustomHooks';
+import { FocusScreen } from './components/04-useRef/FocusScreen';
 import { HookApp } from './HookApp';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- 
-    <MultipleCustomHooks />
-  
-);
+     <FocusScreen />
+ );
 
