ef2f854069f7c9d3eb77e682a2c570edc7146f67
hall9zeha <vmwaretars@gmail.com>
2024-01-17 12:48:15 -0500

    Completed todoReducer tests

--- 03-hook-app/src/tests/07-useReducer/todoReducer.test.js
+++ 03-hook-app/src/tests/07-useReducer/todoReducer.test.js
@@ -9,5 +9,75 @@ describe('Tests in todoReducer', () => {
         const state = todoReducer(todos,{});
         expect(state).toEqual(todos);
     })
+    test('should add one TODO',()=>{
+        const newTodo={
+            id:3,
+            desc:'Learning Android flavors',
+            done:false
+        };
+        const actionAdd={
+            type:'add',
+            payload:newTodo
 
+        }
+        const state = todoReducer(todos, actionAdd);
+        expect(state.length).toBe(3);
+        expect(state).toEqual([...todos,newTodo]);
+    })
+
+    test('should delete a TODO',()=>{
+        const newTodo={
+            id:3,
+            desc:'Learning Android flavors',
+            done:false
+        };
+        const actionAdd={
+            type:'add',
+            payload:newTodo
+
+        }
+
+        const actionDelete={
+            type:'delete',
+            payload:3
+        }
+
+        const state = todoReducer(todos,actionAdd)
+
+        expect(state.length).toBe(3)
+
+        const stateWithTodoDelete = todoReducer(state,actionDelete)
+
+        expect(stateWithTodoDelete.length).toBe(2)
+
+    });
+
+    test('should change done state of TODO',()=>{
+        const newTodo={
+            id:3,
+            desc:'Learning Android flavors',
+            done:false
+        };
+        const actionAdd={
+            type:'add',
+            payload:newTodo
+
+        }
+        const actionDone={
+            type:'done',
+            payload:3
+
+        }
+
+       
+
+        const state = todoReducer(todos,actionAdd)
+
+        expect(state.length).toBe(3)
+        expect(state[2].done).toBe(false)//nuestro TODO insertado está en la tercera posición
+
+        const stateDone = todoReducer(state,actionDone)
+        expect(stateDone[2].done).toBe(true)//Debe retornar estado completado o true
+       
+    });
  })
