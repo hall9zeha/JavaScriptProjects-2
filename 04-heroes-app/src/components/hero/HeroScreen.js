a0d31eaeeda99c0a2f89c5d320afc9356074ee35
hall9zeha <vmwaretars@gmail.com>
2024-01-21 12:02:55 -0500

    Implemented getByHeroId function

--- 04-heroes-app/src/components/hero/HeroScreen.js
+++ 04-heroes-app/src/components/hero/HeroScreen.js
@@ -1,10 +1,19 @@
+import { Navigate, useParams } from "react-router-dom"
+import { getHeroById } from "../../selectors/getHeroById";
 
 
 export const HeroScreen = () => {
+  const {heroId} = useParams();
+  const hero=getHeroById(heroId)
+ 
+  //Si se retorna undefine por alguna razón redireccionamos a la raíz
+  if(!hero){
+    return <Navigate to='/' />
+  }
   return (
     <div>
         <h1>HeroScreen</h1>
-        
+              
     </div>
   )
 }
