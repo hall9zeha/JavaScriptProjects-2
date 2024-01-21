d3c4f19b6cde211abea7a45071a0a378a504ef21
hall9zeha <vmwaretars@gmail.com>
2024-01-21 14:28:22 -0500

    Completed filtered heroes in SearchScreen

--- 04-heroes-app/src/components/search/SearchScreen.js
+++ 04-heroes-app/src/components/search/SearchScreen.js
@@ -1,9 +1,65 @@
 import React from 'react'
+import queryString from 'query-string'
+import { useForm } from '../../hooks/useForm'
+import { HeroCard } from '../hero/HeroCard'
+import { getHeroByName } from '../../selectors/getHeroByName'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 export const SearchScreen = () => {
+
+  const location = useLocation();
+  //Leemos la query de búsqueda contenida en location gracias a la librería "npm install query-string" 
+  const {q=''} = queryString.parse(location.search)
+
+  const [formValues, handleInputChange] = useForm({searchText:q})
+  const {searchText} = formValues;
+
+  const heroesFiltered = getHeroByName(q)
+  const navigate = useNavigate();
+
+
+
+  const handleSearch=(e)=>{
+    e.preventDefault()
+    console.log(searchText)
+    navigate(`?q=${searchText}`)
+  }
+
+ 
+
   return (
-    <div>
-        <h1>SearchScreen</h1>
-    </div>
+    <>
+     <h1>Búsquedas</h1>
+     <hr/>
+     <div className='row'>
+        <div className='col-5'>
+          <h4>Buscar</h4>
+          <form onSubmit={handleSearch}>
+            <input type='text' placeholder='Buscar un héroe'
+                    className='form-control'
+                    name='searchText'
+                    autoComplete='off'
+                    value={searchText}
+                    onChange={handleInputChange}
+                    />
+            <button className='btn btn-outline-primary mt-1 ' type='submit'>
+                Buscar
+            </button>
+          </form>
+        </div>
+        <div className='col-7'>
+          <h4>Resultados</h4>
+          <hr/>
+          {
+            heroesFiltered.map(hero=>(
+              <HeroCard 
+                key={hero.id}
+                {...hero}
+              />
+            ))
+          }
+        </div>
+     </div>
+    </>
   )
 }
