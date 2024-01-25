fb8fc9a5aa658c36011d67a98e3ccbdf188570c6
hall9zeha <vmwaretars@gmail.com>
2024-01-25 12:07:08 -0500

    Completed NavBar tests

--- 04-heroes-app/src/test/components/ui/NavBar.test.js
+++ 04-heroes-app/src/test/components/ui/NavBar.test.js
@@ -3,33 +3,47 @@ import { AuthContext } from '../../../auth/authContext';
 import { AppRouter } from '../../../routers/AppRouter';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import { Navbar } from '../../../components/ui/NavBar';
+import { Types } from '../../../types/Types';
+
+const mockNavigate= jest.fn();//Importante poner el nombre 'mock' delante del hook o funcion que querramos falsea de lo contrario no funcionará
+
+jest.mock('react-router-dom',()=>({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate:() => mockNavigate,
+}))
 
 describe('Tests in <NavBar/> component', () => {
 
-    test('should show successfuly', () => { 
-        const contextValue={
-            dispatch:jest.fn(),
-            user:{
-                name:'Martha',
-                logged:true
-            }
+    const contextValue={
+        dispatch:jest.fn(),
+        user:{
+            name:'Martha',
+            logged:true
         }
-        const wrapper = renderer.create(
-            <AuthContext.Provider value={contextValue}>
-                <MemoryRouter initialEntries={['/']}>
-                    <Routes>
-                        <Route path='/' element={<Navbar/>}/>
-                    </Routes>
-                </MemoryRouter>
-             
-            </AuthContext.Provider>
-        )
+    }
+
+    const wrapper = renderer.create(
+        <AuthContext.Provider value={contextValue}>
+            <MemoryRouter initialEntries={['/']}>
+                <Routes>
+                    <Route path='/' element={<Navbar/>}/>
+                </Routes>
+            </MemoryRouter>
+         
+        </AuthContext.Provider>
+    )
+    test('should show successfuly', () => { 
+       
+        
         expect(wrapper.toJSON()).toMatchSnapshot()
         //const infoUser = wrapper.root.findByProps({className:'nav-item nav-link text-info'});
         const info = wrapper.root.findByType('span')
         expect(info.children[0]).toBe('Martha')
      })
      test('should call logout, call to navigate and dispatch with args',()=>{
-
+        const button = wrapper.root.findByType('button');
+        button.props.onClick()
+        expect(contextValue.dispatch).toHaveBeenCalledWith({type:Types.logout})
+        expect(mockNavigate).toHaveBeenCalledWith('/login',{replace:true})
      })
  })
