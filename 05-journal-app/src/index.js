06b122039b3ddc199c6a0835f1d2cd48e03da29c
hall9zeha <vmwaretars@gmail.com>
2024-01-31 14:46:47 -0500

    Created journal app

---
+++ 05-journal-app/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { JournalApp } from './JournalApp';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+   <JournalApp/>
+);
