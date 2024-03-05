ee8e693f093414c16fa4f9f87edfb9c8d8198d1f
hall9zeha <vmwaretars@gmail.com>
2024-03-05 10:35:55 -0500

    Added RegisterScreen style

--- 05-journal-app/src/components/auth/RegisterScreen.js
+++ 05-journal-app/src/components/auth/RegisterScreen.js
@@ -1,7 +1,52 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const RegisterScreen = () => {
   return (
-    <div>RegisterScreen</div>
+    <>
+    <h3 className='auth__title'>Register</h3>
+    <form>
+    <input
+        type='text'
+        placeholder='Name'
+        name="name"
+        autoComplete='off'
+        className='auth__input'
+      />
+      <input
+        type='text'
+        placeholder='Email'
+        name="email"
+        autoComplete='off'
+        className='auth__input'
+      />
+      <input
+        tyspspe='password'
+        placeholder='Password'
+        name='password'
+        className='auth__input'
+      />
+       <input
+        type='text'
+        placeholder='Confirm password'
+        name="confirm"
+        autoComplete='off'
+        className='auth__input'
+      />
+      <button
+        type='submit'
+        className='btn btn-primary btn-block mb-5'
+      >
+        Register
+      </button>
+  
+      <Link
+        to="/auth/login"
+        className="link"
+      >
+        Already registered?
+      </Link>   
+    </form>
+  </>
   )
 }
