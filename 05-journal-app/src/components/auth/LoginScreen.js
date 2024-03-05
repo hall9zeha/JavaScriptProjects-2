57905a0afd7512170c64645d117390633b519c92
hall9zeha <vmwaretars@gmail.com>
2024-03-05 10:23:17 -0500

    Completed LoginScreen style

--- 05-journal-app/src/components/auth/LoginScreen.js
+++ 05-journal-app/src/components/auth/LoginScreen.js
@@ -1,27 +1,47 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const LoginScreen = () => {
   return (
     <>
-      <h3>Login</h3>
+      <h3 className='auth__title'>Login</h3>
       <form>
         <input
           type='text'
           placeholder='Email'
           name="email"
+          autoComplete='off'
+          className='auth__input'
         />
         <input
-          type='password'
+          tyspspe='password'
           placeholder='Password'
           name='password'
+          className='auth__input'
         />
         <button
           type='submit'
+          className='btn btn-primary btn-block'
         >
           Login
         </button>
-        <hr/>
-        Google
+    
+        <div className='auth__social-networks'>
+          <p>Login with social networks</p>
+              <div  className="google-btn" >
+                  <div className="google-icon-wrapper">
+                      <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/archive/c/c1/20210618182605%21Google_%22G%22_logo.svg" alt="google button" />
+                  </div>
+                  <p className="btn-text">
+                      <b>Sign in with google</b>
+                  </p>
+              </div>
+        </div>
+        <Link
+          to="/auth/register"
+        >
+          Create new account
+        </Link>   
       </form>
     </>
   )
