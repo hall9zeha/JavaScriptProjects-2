35e707c23b0259eb97336e562db91b7e82830c40
hall9zeha <vmwaretars@gmail.com>
2024-03-05 13:34:27 -0500

    Completed NoteScreen styles

--- 05-journal-app/src/components/notes/NoteScreen.js
+++ 05-journal-app/src/components/notes/NoteScreen.js
@@ -5,6 +5,23 @@ export const NoteScreen = () => {
   return (
     <div className='notes__main-content'>
         <NotesAppBar/>
+        <div className='notes__content'>
+            <input 
+                type='text'
+                placeholder='Some awesome'
+                className='notes__title-input'
+                />
+            <textarea
+                placeholder='What happened today'
+                className='notes__textarea'
+            ></textarea>
+            <div className='notes__image'>
+                <img
+                src='https://cdn.pixabay.com/photo/2024/03/03/11/33/balloon-8610226_1280.jpg'
+                alt='nature road'
+                />
+            </div>
+        </div>
     </div>
   )
 }
