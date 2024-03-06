8f39fb0209f482b3f81dfef4c7ed5b5d7aae9cd5
hall9zeha <vmwaretars@gmail.com>
2024-03-06 14:21:25 -0500

    Completed validate register

--- 05-journal-app/src/components/auth/RegisterScreen.js
+++ 05-journal-app/src/components/auth/RegisterScreen.js
@@ -2,11 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { useForm } from '../../hooks/useForm'
 import validator from 'validator'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { removeError, setError } from '../../actions/ui'
 
 export const RegisterScreen = () => {
   const dispatch = useDispatch()
+  // Para obtener los objetos dentro de los estados de redux lo hacemos con useSelector de redux
+  const {msgError} = useSelector(state=>state.ui);
+  
 
   const [values, handleInputChange] = useForm({
     name:'Martha',
@@ -42,6 +45,12 @@ export const RegisterScreen = () => {
     <>
     <h3 className='auth__title'>Register</h3>
     <form onSubmit={handleRegister}>
+      {/* Si el msgError no es nulo entonces mostrará un elemento div con el mesaje */}
+      {
+        msgError && (
+          <div className='auth__alert-error'>{msgError}</div>
+        )
+      }
     <input
         type='text'
         placeholder='Name'
