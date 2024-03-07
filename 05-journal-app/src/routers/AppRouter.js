848d006c86614c57dbae8fcb53d642abefb8334c
hall9zeha <vmwaretars@gmail.com>
2024-03-07 10:55:49 -0500

    Added auth state changed observable

--- 05-journal-app/src/routers/AppRouter.js
+++ 05-journal-app/src/routers/AppRouter.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import {firebase} from '../firebase/firebaseConfig'
 /*
 Ya que react router 5 es todavía muy utilizado este proyecto implementará dicha versión
 para aprender el manejor de las rutas con la misma.
@@ -12,9 +13,25 @@ import{
 } from "react-router-dom";
 import { AuthRouter } from './AuthRouter';
 import { JournalScreen } from '../components/journal/JournalScreen';
+import { useDispatch } from 'react-redux';
+import { login } from '../actions/auth';
 
 
 export const AppRouter = () => {
+
+    const dispatch = useDispatch();
+
+    // Observador que mostrara los estados de autenticación de firebase cuando estos cambien (login or logout)
+    // nos devolverá el objeto correspondiente (user credentials or null)
+    useEffect(()=>{
+        firebase.auth().onAuthStateChanged((user)=>{
+            // Si el usuario no es nulo
+            if(user?.uid){
+                dispatch(login(user.uid,user.displayName));
+            }
+        })
+    }, [])//[] Dependencias vacías para que se ejecute una sola vez si se vuelve a renderizar algún componente
+
   return (
     <Router>
         <div>
