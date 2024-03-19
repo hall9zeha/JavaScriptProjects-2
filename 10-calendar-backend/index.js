4c0c7034a653c970e05ec2c47d1a1a3cca4ddd19
hall9zeha <vmwaretars@gmail.com>
2024-03-19 10:40:48 -0500

    Created first route of backend

--- 10-calendar-backend/index.js
+++ 10-calendar-backend/index.js
@@ -1,1 +1,18 @@
+const express = require('express');
 console.log("Hola world")
+
+//Create express server
+const app = express();
+
+//Routes
+app.get('/',(req,res)=>{
+    res.json({
+        ok:true
+    })
+})
+
+
+//Listen requests
+app.listen(4001,()=>{
+    console.log("Server is running")
+})
