a89b6d2ee39de09c3c5fe96d7a83fe21f5b5788d
hall9zeha <vmwaretars@gmail.com>
2024-03-19 10:53:54 -0500

    Added environment variables file

--- 10-calendar-backend/index.js
+++ 10-calendar-backend/index.js
@@ -4,12 +4,17 @@ console.log("Hola world")
 //Create express server
 const app = express();
 
+
+//Public directory
+app.use(express.static('public')) //El use es un middleware que se ejecuta cuando alguien hace una petición al servidor
+
+
 //Routes
-app.get('/',(req,res)=>{
-    res.json({
-        ok:true
-    })
-})
+// app.get('/',(req,res)=>{
+//     res.json({
+//         ok:true
+//     })
+// })
 
 
 //Listen requests
