78a4248e386ab7287555aeb18695c704fd28001a
hall9zeha <vmwaretars@gmail.com>
2024-03-20 10:22:20 -0500

    Added user model and create first register in mongo

--- 10-calendar-backend/controllers/auth.js
+++ 10-calendar-backend/controllers/auth.js
@@ -1,16 +1,29 @@
+const User = require("../models/User");
 
-const createUser = (req,res)=>{
+const createUser = async(req,res)=>{
 
    
     const {name,email,password} = req.body;
-   
-    res.status(201).json({
-        ok:true,
-        msg:'register',
-        name,
-        email,
-        password
-    })
+    const user = new User(req.body)
+
+    try {
+        //Creamos un registro con un usuario usando mongoose
+        await user.save();
+        //Respuesta en formato json correctamente 201 -> usuario creado
+        res.status(201).json({
+            ok:true,
+            msg:'register',
+           
+        })
+        
+    } catch (error) {
+        console.log(error)
+        //Respuesta en formato json error 500
+        res.status(500).json({
+            ok:false,
+            msg:'Error at create user'
+        })
+    }
 }
 
 const login = (req,res)=>{
