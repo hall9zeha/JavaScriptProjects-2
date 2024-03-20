c91d6543f065322bba9b0ccaeab5d527c4b96179
hall9zeha <vmwaretars@gmail.com>
2024-03-20 10:33:38 -0500

    Validate email with mongoose

--- 10-calendar-backend/controllers/auth.js
+++ 10-calendar-backend/controllers/auth.js
@@ -4,15 +4,28 @@ const createUser = async(req,res)=>{
 
    
     const {name,email,password} = req.body;
-    const user = new User(req.body)
+    
 
     try {
+        //Validamos la existencia de un email con mongosse, desde nuestra instancia de user
+        let user = await User.findOne({email:email});
+
+        if(user){
+            return res.status(400).json({
+                ok:false,
+                msg:'This email is already registered'
+            })
+        }
+        
+        user = new User(req.body)
+
         //Creamos un registro con un usuario usando mongoose
         await user.save();
         //Respuesta en formato json correctamente 201 -> usuario creado
         res.status(201).json({
             ok:true,
-            msg:'register',
+            uid:user.id,
+            name:user.name
            
         })
         
