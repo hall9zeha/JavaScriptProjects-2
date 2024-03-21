e25d9e4241a7d3e4da34cad98a171fecc75a7a28
hall9zeha <vmwaretars@gmail.com>
2024-03-21 12:12:02 -0500

    Completed get events functionality

--- 10-calendar-backend/controllers/events.js
+++ 10-calendar-backend/controllers/events.js
@@ -1,10 +1,16 @@
 const Event = require("../models/Event")
 
 //Get events
-const getEvents = (req, res)=>{
+const getEvents = async (req, res)=>{
+
+    const events = await Event.find()
+    //Rellenamos los datos de usuario correspondiente a la referencia por id que está en nuestra propiedad 'user'
+    //esto lo hará mongoose por nosotros, podemos especificar los campos que queremos en este caso ('name')
+                            .populate('user','name');
     res.status(200).json({
         ok:true,
-        msg:"Eventos cargados"
+        msg:"Eventos cargados",
+        events
     })
 }
 
