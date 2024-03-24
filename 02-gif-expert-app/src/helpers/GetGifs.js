011a8bbd4adc4858a206331c7d5ad0d323e90ebf
hall9zeha <vmwaretars@gmail.com>
2024-03-24 16:26:51 -0500

    Get api_key of environment variable

--- 02-gif-expert-app/src/helpers/GetGifs.js
+++ 02-gif-expert-app/src/helpers/GetGifs.js
@@ -1,5 +1,6 @@
 export  const fetchGifs = async( category) =>{
-    const url=`https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=Your api key`;
+    const api_key = process.env.REACT_APP_API_KEY;
+    const url=`https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=${api_key}`;
     const resp = await fetch(url);
     const {data} = await resp.json();
     
