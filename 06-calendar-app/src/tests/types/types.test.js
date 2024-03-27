fe50f6b25b4ad43030e0145a1d260a4a9f372aac
hall9zeha <vmwaretars@gmail.com>
2024-03-27 09:41:35 -0500

    Created first test for calendar app

---
+++ 06-calendar-app/src/tests/types/types.test.js
@@ -0,0 +1,33 @@
+import { types } from "../../types/types"
+
+describe('Test in types', () => { 
+
+    test('should be equal object types', () => { 
+        expect(types).toEqual({
+            //ui states
+            uiOpenModal:'[ui] Open modal',
+            uiCloseModal:'[ui] Close modal',
+        
+            //event states
+            eventSetActive:'[Event] Set active',
+            eventLogout:'[Event] Event logout',
+        
+            eventStartAddNew:'[Event] Start add new',
+            eventAddNew:'[Event] Add new',
+            eventClearActiveEvent:'[Event] Clear active event',
+            eventUpdated:'[Event] Event update',
+            eventDeleted:'[Event] Event deleted',
+            eventLoaded:'[Event] Events loaded',
+        
+            //auth states
+            authChecking: '[auth] Checking login state',
+            authCheckingFinish:'[auth] Finish checking login state',
+            authStartLogin:'[auth] start login',
+            authLogin:'[auth] Login',
+            authStartRegister:'[auth] Start register',
+            authStartTokenRenew:'[auth] Start token renew',
+            authLogout:'[auth] Logout'
+        })
+     })
+
+ })
